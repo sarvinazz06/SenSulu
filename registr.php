5dbd352c74a50c449465ac3936537233ab4d8986
sarvinazz06 <sarvinoz311220@gmail.com>
2025-05-26 13:52:14 +0500

    registr

---
+++ registr.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <section class="log">
+        <div class="loog">
+            <from action ="api/baylanis.html"method="POST">
+                <h3>Тіркелу</h3>
+                <input name="email"tupe="email" placeholder="Почтаңызды енгізіңіз">
+                <input name="username"tupe="text" placeholder="Аты-жөніңізді енгізіңіз">
+                <input name="password"tupe="passsword" placeholder="Парольді енгізіңіз">
+                <input name="repeatpas"tupe="password" placeholder="Парольді қайталаңыз">
+                <button tupe=jnClick>Жіберу</button>
+</form>
+<a href="baylanis.html">Акауынтқа кіру</a>
+</div>
+</section>
+</body>
+</html>
